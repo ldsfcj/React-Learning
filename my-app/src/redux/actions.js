20551f2a23c88dafb67eea37357240fd09c1f56d
ldsfcj <18907382699@163.com>
2020-06-19 01:37:35 +1000

    import applyMiddleware and thunk async action

--- my-app/src/redux/actions.js
+++ my-app/src/redux/actions.js
@@ -5,6 +5,8 @@
 
 import {DECREMENT, INCREMENT} from "./action-types";
 
+//同步的action都是返回对象
+//异步的action返回的是函数
 export const increment = (number) => ({
     type: INCREMENT,
     data: number
@@ -14,3 +16,13 @@ export const decrement = (number) => ({
     type: DECREMENT,
     data: number
 })
+
+//定义一个异步action
+export const incrementAsync = (number) => {
+    return dispatch => {
+        setTimeout(() => {
+            //1s 之后才去分发一个增加的action
+            dispatch(increment(number));
+        }, 1000)
+    }
+}
