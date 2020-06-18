9276773d20663dc04d98457e7fdf812fe37ddaca
ldsfcj <18907382699@163.com>
2020-06-19 01:54:13 +1000

    import applyMiddleware and thunk async action

--- my-app/src/redux/store.js
+++ my-app/src/redux/store.js
@@ -1,11 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import {counter} from './reducers';
 
 //生成一个store对象
 const store = createStore(
     counter,
-    applyMiddleware(thunk) // 应用上异步的中间件
+    composeWithDevTools(applyMiddleware(thunk)), // 应用上异步的中间件
 ); // 内部会第一次调用reducer函数得到初始state
 
 export default store;
