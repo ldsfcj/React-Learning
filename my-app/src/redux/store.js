ec3faaed10d2d8200d370450713ac87ffab138fd
ldsfcj <18907382699@163.com>
2020-06-19 01:27:06 +1000

    import applyMiddleware and thunk

--- my-app/src/redux/store.js
+++ my-app/src/redux/store.js
@@ -1,7 +1,11 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from "redux-thunk";
 import {counter} from './reducers';
 
 //生成一个store对象
-const store = createStore(counter); // 内部会第一次调用reducer函数得到初始state
+const store = createStore(
+    counter,
+    applyMiddleware(thunk) // 应用上异步的中间件
+); // 内部会第一次调用reducer函数得到初始state
 
-export default store
+export default store;
---
+++ my-app/src_counter3_react-redux/redux/actions.js
@@ -0,0 +1,16 @@
+/*
+包含所有的action creator
+ */
+
+
+import {DECREMENT, INCREMENT} from "./action-types";
+
+export const increment = (number) => ({
+    type: INCREMENT,
+    data: number
+})
+
+export const decrement = (number) => ({
+    type: DECREMENT,
+    data: number
+})
