a05eb621053e8df9051972a98590ba13d25ae88b
ldsfcj <18907382699@163.com>
2020-06-19 00:27:40 +1000

    redux with actions(factory function) with action-types with reducers with store

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
 
 import App from './components/app';
-import {counter} from './redux/reducers';
-
-//生成一个store对象
-const store = createStore(counter); // 内部会第一次调用reducer函数得到初始state
+import store from "./redux/store";
 
 function render(){
     ReactDOM.render(<App store={store}/>, document.getElementById('root'))
 }
-// 初始化渲染
+//初始化渲染
 render();
 //订阅监听（store中的状态变化了，就会自动调用进行重绘）
 store.subscribe(render);
